aa1e01d4555dc2bb443a03d105247948236a6816
Tanveer <tanveeralims@gmail.com>
2019-02-14 11:14:18 +0530

    initial commit

---
+++ build/precache-manifest.11618bc5113e02b86b22beac75a0745d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7ec815022753251cce9b",
+    "url": "/static/js/main.7ec81502.chunk.js"
+  },
+  {
+    "revision": "77fdecab438456e9b2b1",
+    "url": "/static/js/2.77fdecab.chunk.js"
+  },
+  {
+    "revision": "7ec815022753251cce9b",
+    "url": "/static/css/main.0529e3c3.chunk.css"
+  },
+  {
+    "revision": "77fdecab438456e9b2b1",
+    "url": "/static/css/2.7f95de4e.chunk.css"
+  },
+  {
+    "revision": "8259d475a9a5b8734f6eff93b90b32cb",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import SearchArtist from "./components/SearchArtist";
+import ArtistDetails from "./components/ArtistDetails";
+import ArtistEvents from "./components/ArtistEvents";
+import axios from "axios";
+import moment from "moment";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    let artistData = null
+    if (localStorage.getItem("artistDetails")) {
+      artistData = JSON.parse((localStorage.getItem("artistDetails")))
+    }
+    this.state = {
+      name: artistData ? artistData.name : null,
+      artistDetails: artistData ? artistData.artistDetails : null,
+      artistEvents: artistData ? artistData.artistEvents : null,
+      selectedDay: null,
+      loading: false
+    };
+  }
+
+  getUser = event => {
+    const { name, selectedDay, loading, artistEvents } = this.state
+    event.preventDefault();
+    let artistDetails = null;
+    this.setState({ loading: true, selectedDay: null }, () => {
+      axios
+        .get(
+          `https://rest.bandsintown.com/artists/${
+          name
+          }?app_id=09f313e072cd1b192f200fb70df19ea5`
+        )
+        .then(response => {
+          console.log(response)
+          artistDetails = response.data;
+          return axios.get(
+            `https://rest.bandsintown.com/artists/${
+            name
+            }/events?app_id=09f313e072cd1b192f200fb70df19ea5`
+          );
+        })
+        .then(response => {
+          this.setState(
+            {
+              artistDetails,
+              artistEvents: response.data,
+              loading: false
+            },
+            () => {
+              localStorage.setItem('artistDetails', JSON.stringify({
+                artistDetails,
+                artistEvents: this.state.artistEvents,
+                name: name
+              }));
+            }
+          );
+        });
+    })
+  };
+
+  handleSearch = event => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleDayClick = day => {
+    this.setState(prevState => ({
+      selectedDay: day || prevState.selectedDay
+    }));
+  };
+
+  handleAllEventsClick = e => {
+    e.preventDefault();
+    this.setState({
+      selectedDay: null
+    });
+  };
+
   render() {
+    const { selectedDay, artistEvents, name, artistDetails, loading} = this.state;
+    const events = selectedDay
+      ? artistEvents.filter(item => {
+        if (
+          moment(item.datetime).format("YYYY-MM-DD") ===
+          moment(selectedDay).format("YYYY-MM-DD")
+        ) {
+          return item;
+        }
+      })
+      : artistEvents;
+      console.log(events, artistEvents, artistDetails, loading)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <SearchArtist handleSearch={this.handleSearch} name={name} getUser={this.getUser} />
+        {!loading ? 
+           artistDetails ? <Fragment>
+            <ArtistDetails
+              details={artistDetails}
+              events={artistEvents}
+              onDayClick={this.handleDayClick}
+              selectedDay={selectedDay}
+              onAllEventsClick={this.handleAllEventsClick}
+            />
+            <ArtistEvents
+              events={events}
+              selectedDay={
+                selectedDay &&
+                moment(selectedDay).format("MMM DD")
+              }
+            />
+          </Fragment> : name && <p>Now Artist found</p>
+          : <div className="loader" />
+        }
       </div>
     );
   }
